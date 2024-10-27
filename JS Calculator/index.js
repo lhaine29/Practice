9a4380924b070819548addf8ca9abba4ecae4cc4
RoxanneJimenez <roxjimen29@gmail.com>
2024-10-27 11:51:25 +0800

    update

--- JS Calculator/index.js
+++ JS Calculator/index.js
@@ -1,7 +1,10 @@
 const display = document.getElementById("display");
 
-function appenTodisplay(input){
-    display.value += input;
+
+
+
+function appenTodisplay(input){ // This function takes an input parameter and appends it to the current value of the display.
+    display.value += input; // display.value += input;: The += operator appends the input to the existing value of display.value.
 }
 
 function clearDisplay(){
@@ -9,10 +12,10 @@ function clearDisplay(){
 }
 
 function calculate(){
-    try{
-        display.value = eval(display.value);
+    try{ //attempts to evaluate the expression
+        display.value = eval(display.value); //which executes the string as JavaScript code. If there's an error
     }
-    catch{
-        display.value = "Error";
+    catch{ //sets the display to "Error
+        display.value = "Error"; 
     }
 }
