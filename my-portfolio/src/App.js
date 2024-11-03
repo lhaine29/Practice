0ceb8c7c0dbedc342d35c8335ae2dce35d6f69ae
RoxanneJimenez <roxjimen29@gmail.com>
2024-11-03 19:51:45 +0800

    My Portfolio

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyPortfolio from './myportfolio';
+import Portfolio from './portfolio.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MyPortfolio/>
+    </div>
+  );
+}
+
+export default App;
